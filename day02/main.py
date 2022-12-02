51fc7bf9eefb74277434f906d7cc036b5835f196
nathanridding <nathan.ridding@cts.co>
2022-12-02 10:18:59 +0000

    Add day 2

--- day02/main.py
+++ day02/main.py
@@ -17,37 +17,29 @@ VALUES = {
     "Z": 2,
 }
 
+
 def get_data(filename):
     with open(filename) as f:
         return [line.split(" ") for line in f.read().split("\n")]
 
 
 def part_one(player_one, player_two):
-
-    if player_two == player_one: # draw
-        score = 3 + player_two + 1
-    elif player_two == (player_one + 1) % 3: # win
-        score = 6 + player_two + 1
-    elif player_two == (player_one + 2) % 3: # lose
+    if player_two == (player_one - 1) % 3:  # lose
         score = 0 + player_two + 1
-
+    elif player_two == player_one:  # draw
+        score = 3 + player_two + 1
+    elif player_two == (player_one + 1) % 3:  # win
+        score = 6 + player_two + 1
     return score
 
 
 def part_two(player_one, outcome):
-
-    if outcome == 0: # lose
-        player_two = (player_one - 1) % 3
-    elif outcome == 1: # draw
-        player_two = player_one
-    elif outcome == 2: # win
-        player_two = (player_one + 1) % 3
-
+    player_two = (player_one + outcome - 1) % 3
     return player_two + 1 + 3 * outcome
 
 
 def main():
-    data = data = get_data("day02/test-data.txt")
+    data = data = get_data("day02/data.txt")
 
     part_one_score = 0
     part_two_score = 0
