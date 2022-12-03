0812bced50df04064e806b24eaf2888383dcf1eb
nathanridding <nathan.ridding@cts.co>
2022-12-03 14:14:30 +0000

    Update day 3

--- day03/main.py
+++ day03/main.py
@@ -2,16 +2,16 @@
 Day 3: Rucksack Reorganization
 """
 
-def get_data(filename):
+def get_data(filename: str) -> list:
     with open(filename) as f:
         return [list(line) for line in f.read().split("\n")]
 
 
-def convert_letter(letter):
+def convert_letter(letter: str) -> int:
     return ord(letter) - 96 if letter.lower() == letter else ord(letter) - 38
 
 
-def find_duplicate_items(group):
+def find_duplicate_items(group: list) -> list:
     if not group:
         return []
     elif len(group) == 1:
@@ -20,14 +20,14 @@ def find_duplicate_items(group):
     return find_duplicate_items([list(duplicate_items), *group[2:]])
 
 
-def format_data(data, type):
+def format_data(data: list, type: int) -> list:
     if type == 1:
         return [[line[: len(line) // 2], line[len(line) // 2 :]] for line in data]
     elif type == 2:
         return [data[i : i + 3] for i in range(0, len(data), 3)]
 
 
-def get_priority_sum(data):
+def get_priority_sum(data: list) -> int:
     priority_sum = 0
     for group in data:
         duplicate_items = find_duplicate_items(group)
@@ -35,16 +35,13 @@ def get_priority_sum(data):
     return priority_sum
 
 
-def main(filename):
+def main(filename: str) -> tuple:
     data = get_data(filename)
 
     data1 = format_data(data, 1)
     data2 = format_data(data, 2)
 
-    priority_sum1 = get_priority_sum(data1)
-    priority_sum2 = get_priority_sum(data2)
-
-    return priority_sum1, priority_sum2
+    return get_priority_sum(data1), get_priority_sum(data2)
 
 
 if __name__ == "__main__":
