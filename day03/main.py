407584a3d634a44eee6e93e6fe56f5449df22db5
nathanridding <nathan.ridding@cts.co>
2022-12-03 14:15:50 +0000

    Update day 3

--- day03/main.py
+++ day03/main.py
@@ -7,8 +7,11 @@ def get_data(filename: str) -> list:
         return [list(line) for line in f.read().split("\n")]
 
 
-def convert_letter(letter: str) -> int:
-    return ord(letter) - 96 if letter.lower() == letter else ord(letter) - 38
+def format_data(data: list, type: int) -> list:
+    if type == 1:
+        return [[line[: len(line) // 2], line[len(line) // 2 :]] for line in data]
+    elif type == 2:
+        return [data[i : i + 3] for i in range(0, len(data), 3)]
 
 
 def find_duplicate_items(group: list) -> list:
@@ -20,11 +23,8 @@ def find_duplicate_items(group: list) -> list:
     return find_duplicate_items([list(duplicate_items), *group[2:]])
 
 
-def format_data(data: list, type: int) -> list:
-    if type == 1:
-        return [[line[: len(line) // 2], line[len(line) // 2 :]] for line in data]
-    elif type == 2:
-        return [data[i : i + 3] for i in range(0, len(data), 3)]
+def convert_letter(letter: str) -> int:
+    return ord(letter) - 96 if letter.lower() == letter else ord(letter) - 38
 
 
 def get_priority_sum(data: list) -> int:
